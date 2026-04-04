475c8d8c293b8da4606f580a87ea024ef3e6e600
CarlosEmerito <carlos09102007@gmail.com>
2026-04-04 20:03:32 +0000

    fix: migrate to @ducanh2912/next-pwa for ESM compatibility

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig = {
   turbopack: {},
@@ -18,4 +13,9 @@ const nextConfig = {
   },
 };
 
-module.exports = withPWA(nextConfig);
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
